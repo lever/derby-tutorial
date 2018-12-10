163002519ac3f47b12f83f83deae0b00811c967c
David Zhu <hdavidzhu@gmail.com>
2018-12-09 22:45:02 -0800

    Add new comments to the collaborative editor.

--- src/components/collaborative-note-editor/index.js
+++ src/components/collaborative-note-editor/index.js
@@ -1,7 +1,14 @@
 class CollaborativeNoteEditor {
 
   init() {
+    // Get `noteId` from the route param. See:
+    // - ./src/components/collaborative-note-editor/index.js
+    // - https://derbyjs.com/docs/derby-0.10/apps/routes#routes
     let noteId = this.model.root.get('$render.params.noteId')
+
+    // Learn more about `ref` and `scope` here:
+    // - https://derbyjs.com/docs/derby-0.10/models/references
+    // - https://derbyjs.com/docs/derby-0.10/models/paths#scoped-models
     this.$note = this.model.ref('note', this.model.scope(`notes.${noteId}`))
   }
 }
@@ -11,7 +18,13 @@ CollaborativeNoteEditor.prototype.view = __dirname
 
 CollaborativeNoteEditor.load = (model, params, queries, cb) => {
   $note = model.at(`notes.${params.noteId}`)
+
+  // Fetch the remote note document, and subscribe to any changes
+  // - https://derbyjs.com/docs/derby-0.10/models/backends#loading-data-into-a-model
   $note.subscribe((err) => {
+
+    // If the note doesn't exist, create a new one and set it to the following
+    // string:
     $note.createNull('This note is collaborative! Try editing this in two different windows.')
     return cb(err)
   })
