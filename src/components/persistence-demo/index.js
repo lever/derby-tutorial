93da959f8d676ab78420063306378a43477e472c
David Zhu <hdavidzhu@gmail.com>
2018-12-27 16:25:40 -0800

    Add some basic docs

--- src/components/persistence-demo/index.js
+++ src/components/persistence-demo/index.js
@@ -4,33 +4,66 @@ const faker = require('faker')
 class PersistenceDemo {
 
   init() {
-    this.$collectionNames = this.model.at('collectionNames')
-    this.$collectionsByName = this.model.at('collectionsByName')
-    this.$errorMessageByDocId = this.model.at('errorMessageByDocId')
     this.$root = this.model.root
+
+    // [Array<String>]
+    this.$collectionNames = this.model.at('collectionNames')
+
+    // [Object<String, Object<UUID, Object>>]
+    this.$collectionsByName = this.model.at('collectionsByName')
+
+    // [Object<UUID, String>]
+    this.$errorMessageByDocId = this.model.at('errorMessageByDocId')
+
+    // [Object<String, String>]
     this.$statusByCollectionName = this.model.at('statusByCollectionName')
 
+    // [Object<String, Array<Object>>] Map of a list of sorted documents by
+    // their collection name
     this.model.start(
       'docsByCollectionName',
       this.$collectionsByName,
-      (collectionsByName) => _.mapValues(collectionsByName, (docsById) => _.sortBy(docsById, 'id'))
+      (collectionsByName) => {
+        return _.mapValues(
+          collectionsByName,
+          (docsById) => _.sortBy(docsById, 'id')
+        )
+      }
     )
   }
 
   create() {
+    // Fetch/subscribe to some initial collections to demo what they look like
     this._fetchCollection('apples')
     this._subscribeCollection('bananas')
   }
 
   _fetchCollection(collectionName) {
-    this.model.query(collectionName, {}).fetch((err) => {
+    // Define a query to fetch/subscribe to a specific set of documents. '{}'
+    // would return all docs of that collection. Limit the returned docs by
+    // increasing query specificity
+    // @see https://derbyjs.com/docs/derby-0.10/models/queries
+    const query = this.model.query(collectionName, {})
+
+    // After defining the query, we async fetch
+    query.fetch((err) => {
       if (err) return alert(err)
+
+      // Access docs by:
+      console.log("Query results for ${collectionName}:", query.get())
+
+      // They are also present on the root model
+      console.log('$root:', this.$root.get())
+
       this._addCollection(collectionName)
       this.$statusByCollectionName.set(collectionName, 'fetched')
     })
   }
 
   _subscribeCollection(collectionName) {
+    // Subscribing looks just like fetching. After subscribing to docs or
+    // queries, we can now get remote updates. (Fetching won't listen to
+    // changes)
     this.model.query(collectionName, {}).subscribe((err) => {
       if (err) return alert(err)
       this._addCollection(collectionName)
@@ -48,7 +81,9 @@ class PersistenceDemo {
   }
 
   _addDoc(collectionName) {
-    const docId = this.$root.add(collectionName, {name: faker.name.firstName()})
+    const docId = this.$root.add(collectionName, {
+      name: faker.name.firstName()
+    })
   }
 
   _removeDoc(collectionName, docId) {
@@ -56,6 +91,8 @@ class PersistenceDemo {
   }
 }
 
+// When user modifies a document, wait until they finish typing and try to save
+// the result. Surface any errors if we can't save the doc
 PersistenceDemo.prototype._onDocInput = _.debounce(
   function (e, collectionName, docId) {
     try {
@@ -66,6 +103,9 @@ PersistenceDemo.prototype._onDocInput = _.debounce(
       this.$errorMessageByDocId.setDiff(docId, err.message)
     }
   },
+  // FIXME 12/27/18: After saving something modified in a contenteditable, the
+  // cursor resets to the start of the editable area. Debounce to allow user to
+  // finish typing in their changes
   500
 )
 
